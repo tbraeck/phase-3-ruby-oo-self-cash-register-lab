6c973c83b941b07abffe63b22e82c27aad27a413
Tate <tatebraeckel@gmail.com>
2023-03-31 12:27:08 -0500

    commit tree 795456a1dfee2f911e173fce83b117f41d70cd79

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+require 'pry'
 
+
+
+class CashRegister 
+    
+    attr_accessor :total, :items, :discount
+
+    def initialize(discount= 0)
+
+        @total = 0
+        @items = []
+        @discount = discount 
+
+    end
+
+    def add_item(title, price)
+# new_item
+
+    end
+
+
+
+puts 'Im here'
+end
+
+new_item = CashRegister.new(20)
+new_item
+# binding.pry
